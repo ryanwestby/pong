541eb88a3625a907bc7cbbc558e3e39c55952894
Ryan Westby <jrwestby13@gmail.com>
2015-05-26 21:09:41 -0700

    Created Game, Paddle, Ball classes, and draw methods

--- pong.py
+++ pong.py
@@ -1,20 +1,96 @@
 import pygame, sys
 from pygame.locals import *
 
-fps_clock = pygame.time.Clock()
-fps = 40
+BLACK = (0,0,0)
+WHITE = (255,255,255)
 
 window_width = 400
 window_height = 300
 
+display_surf = pygame.display.set_mode((window_width,window_height))
+
+fps_clock = pygame.time.Clock()
+fps = 40
+
+class Game():
+	def __init__(self,line_thickness=10,speed=4):
+		self.line_thickness = line_thickness
+		self.speed = speed
+		self.score = 0
+		
+		# Initiate variables and set starting positions
+		ball_x = int(window_width/2 - self.line_thickness/2)
+		ball_y = int(window_height/2 - self.line_thickness/2)
+		self.ball = Ball(ball_x,ball_y,self.line_thickness,self.line_thickness,self.speed)
+		
+		self.paddles = {}
+		paddle_height = 50
+		paddle_width = self.line_thickness
+		user_paddle_x = 20
+		computer_paddle_x = window_width - paddle_width - 20
+		self.paddles['user'] = Paddle(user_paddle_x, paddle_width, paddle_height)
+		self.paddles['computer'] = AutoPaddle(computer_paddle_x, paddle_width, paddle_height, self.ball, self.speed)
+	
+	# Draw the arena for the game to be played in
+	def draw_arena(self):
+		display_surf.fill(BLACK)
+		
+		# Draw outline of arena
+		pygame.draw.rect(display_surf, WHITE, ((0,0),(window_width,window_height)),self.line_thickness*2)
+		
+		# Draw center line
+		pygame.draw.line(display_surf, WHITE,(int(window_width/2),0),(int(window_width/2),window_height),int(self.line_thickness/4))
+		
+	def update(self):
+		self.draw_arena()
+		self.ball.draw()
+		self.paddles['user'].draw()
+		self.paddles['computer'].draw()
+
+class Paddle(pygame.sprite.Sprite):
+	def __init__(self,x,w,h):
+		self.x = x
+		self.w = w
+		self.h = h
+		self.y = int(window_height/2 - self.h/2)
+		
+		# Create rectangle for paddle
+		self.rect = pygame.Rect(self.x, self.y, self.w, self.h)
+		
+	# Draw the paddle
+	def draw(self):
+		pygame.draw.rect(display_surf, WHITE, self.rect)
+
+class AutoPaddle(Paddle):
+	def __init__(self,x,w,h,ball,speed):
+		super(self.__class__,self).__init__(x,w,h)
+		self.ball = ball
+		self.speed = speed
+
+class Ball(pygame.sprite.Sprite):
+	def __init__(self,x,y,w,h,speed):
+		self.x = x
+		self.y = y
+		self.w = w
+		self.h = h
+		self.speed = speed
+		self.dir_x = -1
+		self.dir_y = -1
+		
+		self.rect = pygame.Rect(self.x,self.y,self.w,self.h)
+	
+	# Draw the ball
+	def draw(self):
+		pygame.draw.rect(display_surf,WHITE,self.rect)
+		
 # Main function
 def main():
 	pygame.init()
-	global display_surf
 	
-	display_surf = pygame.display.set_mode((window_width,window_height))
 	pygame.display.set_caption('Pong')
 	pygame.mouse.set_visible(0) # make cursor invisible
+	
+	game = Game()
 	
 	while True: # main game loop
 		for event in pygame.event.get():
@@ -22,6 +98,7 @@ def main():
 				pygame.quit()
 				sys.exit()
 		
+		game.update()
 		pygame.display.update()
 		fps_clock.tick(fps)
 
